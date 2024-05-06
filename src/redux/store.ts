daffbeeaf92cef5b12c9a4bd637e9e3bb23ce60a
Gaby1994-77 <101013397+Gaby1994-77@users.noreply.github.com>
2024-05-06 20:47:45 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,33 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import authReducer from './authReducer';
 import savedPostsReducer from './savedPostsReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['auth', 'savedPosts'],
+};
 
 const rootReducer = combineReducers({
   auth: authReducer,
   savedPosts: savedPostsReducer,
 });
 
-type RootState = ReturnType<typeof rootReducer>;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default configureStore({
-  reducer: rootReducer,
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],
+      },
+    }),
 });
-export type {RootState};
+
+const persistor = persistStore(store);
+
+export default store;
+export {persistor};
